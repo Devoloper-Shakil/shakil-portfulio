8e8b6c118c0fdff72baf9326bc8f9f7d174316c8
shakil hasan <69521109+Devoloper-Shakil@users.noreply.github.com>
2020-09-07 23:09:18 -0700

    Add files via upload

---
+++ js/castum.js
@@ -0,0 +1,14 @@
+ 
+ <script>
+ 
+ var typed3 = new Typed('#typed3', {
+    strings: ['WEB DESIGNER',
+     'YOUTUBER', 
+     'WEB DESIGN AND DEVOLOPER'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+    
+  </script>
